b386caf180a7c025c7b0526fc6162110fd95cb0d
Axi <axidreamdragon@gmail.com>
2024-10-31 15:01:58 +0100

    Create FadingScreen

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,44 @@
-import React from 'react';
+import FadingScreen from './components/FadingScreen/FadingScreen';
+import blahaj from './assets/roomImages/blahaj.jpg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      Under construction
+      <h1 style={{
+        color: 'white',
+        position: 'absolute',
+        top: '33vh',
+        left: '16vh',
+        zIndex: 5,
+        fontSize: '10vh',
+        textShadow: '0 0 25px black'
+      }}>hey! i'm axi
+        <p style={{
+          fontSize: '2vh',
+        }}>
+          (this website is under construction)
+        </p>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <p style={{
+          fontSize: '5vh',
+        }}>
+          i'm a software engineer and i like to make things
+          <br />
+          i also do web development
+        </p>
+      </h1>
+      <FadingScreen color={'black'} />
+      <img className="fullImage" src={blahaj} />
     </div>
   );
 }
---
+++ src/components/FadingScreen/FadingScreen.tsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from 'react';
+import styles from './FadingScreen.module.css';
+
+
+type Props = {
+	color: string;
+}
+
+export default function FadingScreen({ color }: Props) {
+	const [opacity, setOpacity] = useState<number>(1);
+	const body: HTMLElement = document.querySelector('body') as HTMLElement;
+
+	useEffect(() => {
+		//TODO: Account for using arrow keys
+		const handleScroll = (event: Event) => {
+			let delta: number = 0;
+
+			if (event instanceof WheelEvent) {
+				delta = event.deltaY / 1500;
+			}
+
+			delta = Math.max(0, delta);
+			const newOpacity = Math.min(Math.max(opacity - delta, 0), 1);
+			setOpacity(newOpacity);
+
+			if (opacity === 0) {
+				body.style.overflow = 'auto';
+			} else {
+				body.style.overflow = 'hidden';
+			}
+		}
+
+		body.style.overflow = 'hidden';
+
+		window.addEventListener('wheel', handleScroll, { passive: false });
+
+		return () => {
+			window.removeEventListener('wheel', handleScroll);
+		}
+	}, [opacity]);
+
+	return (
+		<div className={styles.container}>
+			<div
+				className={styles.screen}
+				style={{ opacity, backgroundColor: color }}
+			/>
+		</div>
+	);
+}
