0f48a0bd5ecb3e9908c8c643b364960c8936a9e7
Axi <axidreamdragon@gmail.com>
2024-11-17 17:40:03 +0100

    Create basic curve animation

--- src/components/ProjectImage/ProjectImage.tsx
+++ src/components/ProjectImage/ProjectImage.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef, useState } from 'react';
 import styles from './ProjectImage.module.css';
 
 export enum FillType {
@@ -13,12 +14,45 @@ type Props = {
 }
 
 const ProjectImage: React.FC<Props> = ({ imageSource, gridRow, gridColumn, fillType = FillType.WIDTH }) => {
+	const [isVisible, setIsVisible] = useState(false);
+	const ref = useRef<HTMLDivElement>(null);
+
+	useEffect(() => {
+		const observer = new IntersectionObserver((entries) => {
+			entries.forEach(entry => {
+				setIsVisible(entry.isIntersecting);
+			});
+		},
+			{
+				threshold: 0.5,
+			}
+		);
+
+		if (ref.current) {
+			observer.observe(ref.current);
+		}
+
+		return () => {
+			if (ref.current) {
+				observer.unobserve(ref.current);
+			}
+		};
+
+	}, []);
+
 	return (
-		<div className={styles.imageWrapper} style={{ gridRow, gridColumn }} >
+		<div className={styles.imageWrapper}
+			ref={ref}
+			style={{
+				gridRow,
+				gridColumn,
+			}} >
 			<img src={imageSource} className={styles.projectImage}
 				style={{
 					[fillType]: '100%',
 					objectFit: 'cover',
+					transform: isVisible ? '' : 'translateX(-100vw) translateY(100vh) rotate(45deg)',
+					transition: 'all 1s cubic-bezier(0.175, 0.885, 0.32, 1.275)',
 				}}
 			/>
 		</div>
