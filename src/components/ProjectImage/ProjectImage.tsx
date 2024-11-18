2e8098d26b194df35791fdd87b3618c64d3d8169
Axi <axidreamdragon@gmail.com>
2024-11-18 21:51:04 +0100

    Implement dynamic slide-ins

--- src/components/ProjectImage/ProjectImage.tsx
+++ src/components/ProjectImage/ProjectImage.tsx
@@ -13,7 +13,7 @@ type Props = {
 	fillType: FillType;
 }
 
-const rotationVariation = 5;
+const rotationVariation = 7;
 
 const isOnDesktop = () => {
 	const userAgent = navigator.userAgent.toLowerCase();
@@ -25,19 +25,45 @@ const isOnDesktop = () => {
 	}
 }
 
-function getRandomAnimation(): string {
-	const baseTime = 0.75;
-	const time = Math.random() * baseTime + (baseTime);
-
-	return `all ${time}s ease-in-out`;
-}
-
 const ProjectImage: React.FC<Props> = ({ imageSource, gridRow, gridColumn, fillType = FillType.WIDTH }) => {
 	const [isVisible, setIsVisible] = useState(false);
+	const [hiddenTranslation, setHiddenTranslation] = useState('');
+	const [animationTime, setAnimationTime] = useState(0);
 	const ref = useRef<HTMLDivElement>(null);
-	const animationRef = useRef<string>(getRandomAnimation());
 	const visibleRotationRef = useRef<number>(Math.random() * rotationVariation - rotationVariation / 2);
-	const invisibleRotationRef = useRef<number>(15 * Math.random() * rotationVariation);
+	const hiddenRotationRef = useRef<number>(15 * Math.random() * rotationVariation);
+
+	useEffect(() => {
+		if (ref.current) {
+			const baseTime = 1.3;
+			const randomTimeAmplitude = 0.5;
+			const centerClosenessMultiplier = 0.5;
+			const yAmplitude = 100;
+
+			const rect = ref.current.getBoundingClientRect();
+			const x = rect.x + rect.width / 2;
+
+			let translateX = 0;
+			let translateY = Math.random() * yAmplitude - yAmplitude / 2;
+
+			if (x < window.innerWidth / 2) {
+				translateX = -100;
+			} else {
+				translateX = 100;
+			}
+
+			setHiddenTranslation(`translateX(${translateX}vw) translateY(${translateY}vh)`);
+
+			const centerCloseness = Math.abs(x - window.innerWidth / 2) / (window.innerWidth / 2);
+			console.log(x, centerCloseness, imageSource);
+
+			setAnimationTime(baseTime +
+				centerCloseness * centerClosenessMultiplier
+				+ (Math.random() * randomTimeAmplitude - randomTimeAmplitude / 2));
+		}
+
+		console.log(hiddenTranslation);
+	}, [ref.current]);
 
 	useEffect(() => {
 		const observer = new IntersectionObserver((entries) => {
@@ -59,7 +85,6 @@ const ProjectImage: React.FC<Props> = ({ imageSource, gridRow, gridColumn, fillT
 				observer.unobserve(ref.current);
 			}
 		};
-
 	}, []);
 
 	return (
@@ -74,8 +99,8 @@ const ProjectImage: React.FC<Props> = ({ imageSource, gridRow, gridColumn, fillT
 					[fillType]: '100%',
 					objectFit: 'cover',
 					transform: isVisible ? `rotate(${visibleRotationRef.current}deg)`
-						: `translateX(-100vw) translateY(100vh) rotate(${invisibleRotationRef.current}deg)`,
-					transition: animationRef.current,
+						: `${hiddenTranslation} rotate(${hiddenRotationRef.current}deg)`,
+					transition: `all ${animationTime}s ease-in-out`,
 				}}
 			/>
 		</div>
