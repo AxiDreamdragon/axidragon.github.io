41c8c6f9a2bf3270e2b94e917ea709003e39e863
Axi <axidreamdragon@gmail.com>
2025-08-25 20:22:04 +0200

    Update intro

--- src/components/screens/IntroScreen.tsx
+++ src/components/screens/IntroScreen.tsx
@@ -10,16 +10,13 @@ function IntroScreen() {
 		<ImageSlide imageSource={background}>
 			<div className={styles.wrapper}>
 				<div className={styles.container}>
-					<p>Hi! I'm axi*! The asterisk doesn't actually mean anything.
-						It was based on 'GOLF le FLEUR*' - I thought the asterisk looked neat, so I stole it for myself.</p>
-					<p>A couple years ago, I started out as a Game Artist
-						at the University of Arts Utrecht.</p>
-					<p>Since then, I've gotten in touch with coding in programs such as Processing and Unity,
-						but also various aspects of software and web development.</p>
-					<p>Now, I love making a variety of programs to make life just a little better.</p>
-					<p>Aside from programming, I also enjoy drawing cute critters, pretending to be a
-						cartoon dragon online, taking forest walks and
-						trying plenty of new things. I've also been trying my hand at cooking recently!</p>
+					<p>Hi! I'm axi*! (The asterisk doesn't mean anything, to be honest - but it looks neat, right?)</p>
+					<p>A couple years ago, I made the decision to apply as a Game Artist
+						at the University of Arts Utrecht. I got a spot. I only realize now  how lucky I was to make it in.</p>
+					<p>Funnily enough though, although I thought I <i>really</i> wanted to be a game artist, one game development lesson where I got in touch with Processing changed my life. I loved (still do, for the record!) the problem-solving aspect of coding!</p>
+					<p>I stopped being a game artist, and became a game designer. I learnt C# and Unity, but also branched out to Godot, Python, Java, and what I now consider my specialty: web development!</p>
+					<p>Combining this with the creative skills I've practiced and learnt over the years, I now create unique web-apps, exploring what a bunch of lines of TypeScript and HTML can accomplish.</p>
+					<p>My game artist background is still here, though! In my free time (and if I'm not programming), I enjoy drawing, 3D modeling, cooking, and being a cartoon dragon online. I've also been getting a bit more into maker electronics and reading!</p>
 				</div>
 				<img src={axi} alt="Axi" className={styles.axiImage} />
 			</div>
