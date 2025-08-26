171b7002757fc1267765b4e851faeb3382f924d6
Axi <axidreamdragon@gmail.com>
2025-08-26 12:23:35 +0200

    Add random backgrounds to portfolio items

--- src/components/Project/ProjectSlide/ProjectSlide.tsx
+++ src/components/Project/ProjectSlide/ProjectSlide.tsx
@@ -4,9 +4,30 @@ import LogoLink from "@components/LogoLink/LogoLink";
 
 import styles from './ProjectSlide.module.scss';
 import { ProjectData } from "@/hooks/projects";
-import backgroundImage from '@/assets/nature/texel1.webp';
 import ProjectDescription from "../ProjectDescription/ProjectDescription";
 import ProjectItem from "../ProjectItem/ProjectItem";
+import { useState } from "react";
+
+import bg1 from '@/assets/nature/texel1.webp';
+import bg2 from '@/assets/nature/texel2.webp';
+import bg3 from '@/assets/nature/texel3.webp';
+import bg4 from '@/assets/nature/texel4.webp';
+import bg5 from '@/assets/nature/texel5.webp';
+import bg6 from '@/assets/nature/boat1.webp';
+
+
+const allBackgrounds = [bg1, bg2, bg3, bg4, bg5, bg6];
+let availableBackgrounds = [...allBackgrounds];
+
+const getBackground = (): string => {
+	if (availableBackgrounds.length === 0) {
+		availableBackgrounds = [...allBackgrounds];
+	}
+
+	const randomIndex = Math.floor(Math.random() * availableBackgrounds.length);
+	return availableBackgrounds.splice(randomIndex, 1)[0];
+}
+
 
 const ProjectSlide = ({
 	id,
@@ -17,7 +38,7 @@ const ProjectSlide = ({
 	year,
 	githubLink = '',
 	webLink = '' }: ProjectData) => {
-	console.log(name, media);
+	const [backgroundImage, _] = useState<string>(getBackground());
 
 	return (
 		<ImageSlide imageSource={backgroundImage} id={id}>
